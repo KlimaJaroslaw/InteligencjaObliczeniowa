21ecfb5dd8b74db86a4802959e0fa02f563f864d
pknot212121 <pknot@student.agh.edu.pl>
2026-04-01 11:16:53 +0200

    Add problems to test.py

--- proj02/aipython/test.py
+++ proj02/aipython/test.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 def test_blocks_world():
     print("\n--- Testowanie Świata Klocków (blocks2 - 4 klocki) ---")
     print(SearcherMPP(Forward_STRIPS(blocks2)).search())
-    
+    print("\n--- Jakiś problem z robotem nvm ---")
+    print(SearcherMPP(Forward_STRIPS(problem1)).search())
 
 if __name__ == "__main__":
     test_blocks_world()
